fde9002db2fc0a8268b54b7df52bf486e09f2cc9
finnfi <finn.gross.maurer@gmail.com>
2022-03-31 13:56:33 +0200

    Working KNN with datasets

--- Scripts/KNN.py
+++ Scripts/KNN.py
@@ -1,29 +1,95 @@
 
+import numpy as np
 
-class KNN_classifier:
-    def __init__(self,training_set, features, k):
+class KNNClassifier:
+    def __init__(self,training_set, features, k, should_normalise):
         '''
         training_set: variable of TrainingSet
         features: array of feature strings
         k: int 
         '''
         self.k = k
+        self.features = features
         self.dim = len(features)
-        self.num_points = 0
+        self.features_mean_and_sd = [] # array of tuple (mean, sd)
         self.training_set = training_set
-        for cls in traning_set.__dict__.values():
-            num_points += len(cls)
-        self.points = np.zeros(num_points,dim)
-        self.index = array(*SongFeatures)
-        i = 0
-        for cls in traning_set.__dict__.values():
-            for song in cls:
-                for feature in features:
-                    
-    
-    def normalise():
-        var = np.variance(self.points(:,1))
-
-    def classify(x):
+        self.num_points = 0
+        for cls in training_set.__dict__.values():
+            self.num_points += len(cls)
+        self.points = np.zeros([self.num_points,self.dim])
+        self.index_to_song = [None]*self.num_points
         
-        pass
+        #Extract feature values
+        i = 0
+        for cls in training_set.__dict__.values():
+            for song in cls:
+                self.index_to_song[i] = song
+                j = 0
+                for feature in features:
+                    self.points[i,j] = song.__dict__[feature]
+                    j += 1
+                i += 1
+        
+        self.is_normalised = False
+        if should_normalise:
+            self.normalise()
+            
+
+    def normalise(self):
+        '''
+        Normalises features using z-score normalisation
+        '''
+        self.is_normalised = True
+        for i in range(self.dim):
+            mean = np.mean(self.points[:,i])
+            var = np.var(self.points[:,i])
+            sd = np.sqrt(var)
+            self.points[:,i] = (self.points[:,i]-mean)/sd
+            self.features_mean_and_sd.append((mean,sd))
+
+    def classify(self, x):
+        '''
+        input: x of type np.array with dim = k (NOT normalised)
+        output: genre string
+        '''
+        #normalise input if normalised
+        if self.is_normalised:
+            for i in range(self.dim):
+                x[i] = (x[i]-self.features_mean_and_sd[i][0])/self.features_mean_and_sd[i][1]
+        # Calculate distances
+        difference = self.points-x
+        distances = np.sum(difference*difference,axis=1)
+        # k smallest distances: 
+        ind = np.argpartition(distances, self.k)[:self.k] #function gives array of indexes
+
+        # Find genres 
+        genres = []
+        for i in ind:
+            song = self.index_to_song[i]
+            genres.append(song.Genre)
+        
+        # Return the most common or the closest one if we got 5 different genres
+        if len(set(genres)) == len(genres):
+            k_nearest_distances = distances[ind]
+            nearest_index = k_nearest_distances.argmin()
+            return genres[nearest_index]
+        else: 
+            return max(set(genres), key = genres.count)
+    
+    def classify_song(self, song):
+        '''
+        input: song of type SongFeatures
+        output: genre string
+        '''
+        x = np.zeros(self.dim)
+        i = 0
+        for feature in self.features:
+            x[i] = song.__dict__[feature]
+            i += 1
+        return self.classify(x)
+
+
+
+
+
+        
--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,7 @@
 from data_extraction import TrainingSet, TestSet
 from plot_histogram import plot_histogram
 from song_features import readGenreClassData
+from KNN import KNNClassifier
 
 import matplotlib.pyplot as plt
 
@@ -11,12 +12,16 @@ songs_dict = readGenreClassData("Data\GenreClassData_30s.txt")
 training_set = TrainingSet(songs_dict)
 test_set = TestSet(songs_dict)
 
+#Plotting of features
+# axs1 = plot_histogram(training_set,"spectral_rolloff_mean")
+# axs2 =plot_histogram(training_set,"mfcc_1_mean")
+# axs3 =plot_histogram(training_set,"spectral_centroid_mean")
+# axs4 =plot_histogram(training_set,"tempo")
+# plt.show()
 
-axs1 = plot_histogram(training_set,"spectral_rolloff_mean")
-axs2 =plot_histogram(training_set,"mfcc_1_mean")
-axs3 =plot_histogram(training_set,"spectral_centroid_mean")
-axs4 =plot_histogram(training_set,"tempo")
+#Create KNN object
 
-plt.show()
+nn_5 = KNNClassifier(training_set, ["spectral_rolloff_mean","mfcc_1_mean","spectral_centroid_mean","tempo"], 5, True)
 
+print("Right genre: ", songs_dict[234].Genre, ", Classified genre: ", nn_5.classify_song(songs_dict[234]))
 
